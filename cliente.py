ed8721664f4f261b1caed1d0a79a01e108a05a3f
Andres Lagos <lagosfelipe56@gmail.com>
2023-09-05 17:28:54 -0500

    Se actualiza la forma de enviar mensajes en el socket

--- cliente.py
+++ cliente.py
@@ -1,13 +1,38 @@
 import socket
-
-HOST = "127.0.0.1" # Direccion del servidor
+import time
+import pickle
+HOST = "127.0.0.1" # Direccion del servidor direccion propia
 PORT = 65123 # > 1023 (Puerto de envío)
+HEADER = 10 # Tamaño del encabezado que indica la longitud de los datos serializados
 
+# Crear un socket usando IPv4 (AF_INET) y TCP (SOCK_STREAM)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Vincular el socket al HOST y al PORT especificados
     s.connect((HOST, PORT))
 
-    s.sendall(b"Hola mundo")
+    # Vincular el socket al HOST y al PORT especificados
+    names = [100, 
+             'Maria', 
+             'Rodrigo', 
+             3.1416, 
+             'Juan', 
+             'Nabucondonsor', 
+             'Sandro',
+             (1+3j)]
 
-    data = s.recv(1024)
+    # Serializar la lista "names" utilizando pickle
+    data_serial = pickle.dumps(names)
+    # Obtener la longitud de los datos serializados y convertirla en una cadena
+    data_len = str(len(data_serial))
 
-print("Recibido" , repr(data))
+    # Crear una cadena de encabezado que tenga un ancho fijo de HEADER (rellenado con espacios en blanco)
+    # para indicar la longitud de los datos serializados
+    data = bytes(f"{data_len:<{HEADER}}", 'utf-8') + data_serial
+    
+    print(data)
+    # Enviar los datos al servidor a través del socket
+    s.send(data)
+        
+        
+    # Esperar 1 segundo (esto es opcional y se utiliza aquí para asegurarse de que el servidor tenga tiempo para recibir los datos)
+    time.sleep(1) 
--- servidor.py
+++ servidor.py
@@ -1,19 +1,41 @@
 import socket
-
-HOST = "127.0.0.1" #Direccion loopback
+import pickle
+HOST = "192.168.137.9" #Direccion loopback direccion propia pc
 PORT = 65123 # > 1023 (Puerto de escucha)
+HEADER = 10 # Tamaño del encabezado que indica la longitud de los datos serializados
 
+# Crear un socket usando IPv4 (AF_INET) y TCP (SOCK_STREAM)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Vincular el socket al HOST y al PORT especificados
     s.bind((HOST, PORT))
+    # Comenzar a escuchar conexiones entrantes
     s.listen()
+    # Aceptar una conexión entrante y obtener la conexión y la dirección del cliente
     conn, addr = s.accept()
 
-    with conn:
-        print(f"Conectado a {addr}:")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            
-            conn.sendall(data)
+    try:
+        with conn:
+            # Imprimir que se ha conectado al cliente
+            print(f"Conectado a {addr}:{addr[1]}")
+            # Bucle para recibir datos continuamente
+            while True:
+                # Recibir el encabezado que contiene la longitud de los datos
+                data_len = conn.recv(HEADER)
+                # Si no se recibe ningún dato, salir del bucle
+                if not data_len:
+                    break
+                else:
+                    # Inicializar una variable para almacenar los datos recibidos
+                    data = b''
+                    # Recibir los datos en fragmentos hasta que se reciba la cantidad especificada por data_len
+                    data += conn.recv(int(data_len))
+                    # Deserializar los datos utilizando pickle
+                    data_deserial = pickle.loads(data)
+                    # Imprimir los datos deserializados y su tipo
+                    print(data_deserial)
+                    print(type(data_deserial))
+    except KeyboardInterrupt: # Capturar una interrupción de teclado (CTRL + C)
+        pass
+    # Imprimir un mensaje de cierre de conexión una vez que se sale del bucle
+print("Cerrando conexion")
 
